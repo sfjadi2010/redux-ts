1a20925ee97b0ee2d8735d786e7e0e4d57f2492e
Salman Farsi <sfjadi2010@live.com>
2025-07-17 11:47:05 -0400

    Refactor App component to integrate Redux Provider; remove "Hello World" message and restructure reducers

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { Provider } from 'react-redux';
+import { store } from './state';
+
 import React from 'react';
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <>
-      <h1>Hello World</h1>
+      <Provider store={store}>
+      </Provider>
     </>
   )
 }
